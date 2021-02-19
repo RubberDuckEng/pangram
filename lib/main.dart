959326da7cf258f45a40051aa636c8befdeaf759
Adam Barth <abarth@chromium.org>
2021-02-18 20:31:27 -0800

    Add a basic pangram layout

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'dart:math';
 
 // A, B, C, D, E, F, G
 
@@ -23,40 +24,134 @@ void main() {
 }
 
 class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Pangram Game',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
+        primarySwatch: Colors.amber,
       ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
+      home: MyHomePage(title: 'Pangram'),
     );
+  }
+}
+
+class HexagonPainter extends CustomPainter {
+  final Color color;
+
+  HexagonPainter({required this.color});
+
+  @override
+  void paint(Canvas canvas, Size size) {
+    var radius = size.shortestSide / 2.0;
+    var center = size.center(Offset.zero);
+
+    const angleIncrement = pi / 3.0;
+    const count = 6;
+    List<Offset> points = [];
+    for (int i = 0; i < count; ++i) {
+      var angle = i * angleIncrement;
+      points.add(center + Offset(cos(angle), sin(angle)) * radius);
+    }
+
+    var path = Path();
+    path.addPolygon(points, true);
+    var paint = Paint();
+    paint.color = color;
+    canvas.drawPath(path, paint);
+  }
+
+  @override
+  bool shouldRepaint(covariant HexagonPainter oldDelegate) {
+    return oldDelegate.color != color;
+  }
+}
+
+class PangramTile extends StatelessWidget {
+  final String letter;
+  final bool isCenter;
+
+  PangramTile({Key? key, required this.letter, required this.isCenter})
+      : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    Color color =
+        isCenter ? Colors.deepOrange.shade500 : Colors.yellow.shade800;
+    return Stack(alignment: AlignmentDirectional.center, children: [
+      SizedBox(
+        height: 75.0,
+        width: 75.0,
+        child: CustomPaint(painter: HexagonPainter(color: color)),
+      ),
+      Text(letter),
+    ]);
+  }
+}
+
+enum _PangramSlot {
+  center,
+  first,
+  second,
+  third,
+  fourth,
+  fifth,
+  sixth,
+}
+
+class PangramLayout extends MultiChildLayoutDelegate {
+  @override
+  void performLayout(Size size) {
+    var centerSize =
+        layoutChild(_PangramSlot.center, BoxConstraints.loose(size));
+    positionChild(_PangramSlot.center,
+        size.center(Offset.zero) - centerSize.center(Offset.zero));
+
+    var radius = size.shortestSide / 2.0;
+    const angleIncrement = pi / 3.0;
+    const satelliteCount = 6;
+    for (int i = 0; i < satelliteCount; ++i) {
+      var slot = _PangramSlot.values[i + 1]; // The center slot is index 0.
+      var satelliteSize = layoutChild(slot, BoxConstraints.loose(size));
+      var angle = i * angleIncrement;
+      var satelliteOffset = size.center(Offset.zero) +
+          Offset(cos(angle), sin(angle)) * radius -
+          satelliteSize.center(Offset.zero);
+      positionChild(slot, satelliteOffset);
+    }
+  }
+
+  Size getSize(BoxConstraints constraints) => Size(200, 200);
+
+  @override
+  bool shouldRelayout(covariant MultiChildLayoutDelegate oldDelegate) => false;
+}
+
+class PangramGame extends StatelessWidget {
+  final Game game;
+
+  PangramGame({Key? key, required this.game}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    var children = [
+      LayoutId(
+          id: _PangramSlot.center,
+          child: PangramTile(letter: game.center, isCenter: true))
+    ];
+    for (var i = 0; i < game.otherLetters.length; ++i) {
+      var letter = game.otherLetters[i];
+      children.add(LayoutId(
+          id: _PangramSlot.values[i + 1],
+          child: PangramTile(letter: letter, isCenter: false)));
+    }
+    return CustomMultiChildLayout(
+        delegate: PangramLayout(), children: children);
   }
 }
 
 class MyHomePage extends StatefulWidget {
   MyHomePage({Key? key, this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
   final String? title;
 
   @override
@@ -64,65 +159,27 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
+  Game _game = new Game(
+      center: "A",
+      otherLetters: ["B", "C", "D", "E", "F", "G"],
+      validWords: ["FADE"]);
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
       appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
         title: Text(widget.title!),
       ),
       body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
         child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
+            PangramGame(game: _game),
           ],
         ),
       ),
       floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
+        onPressed: null,
         tooltip: 'Increment',
         child: Icon(Icons.add),
       ), // This trailing comma makes auto-formatting nicer for build methods.
