1b9fad3f078e8d41f6aa434c36bbfa7467539832
Eric Seidel <github@eseidel.com>
2021-03-11 19:23:54 -0800

    Add a NumberOfAnswers graph

--- lib/stats.dart
+++ lib/stats.dart
@@ -4,15 +4,9 @@ import 'package:fl_chart/fl_chart.dart';
 import 'package:pangram/board_stats.dart';
 import 'package:http/http.dart' as http;
 
-class MaxScoreAllPuzzles extends StatefulWidget {
+class MaxScoreAllPuzzles extends StatelessWidget {
   final List<Bucket> maxScores;
   MaxScoreAllPuzzles(this.maxScores);
-
-  @override
-  State<StatefulWidget> createState() => MaxScoreAllPuzzlesState();
-}
-
-class MaxScoreAllPuzzlesState extends State<MaxScoreAllPuzzles> {
   @override
   Widget build(BuildContext context) {
     return BarChart(
@@ -22,22 +16,82 @@ class MaxScoreAllPuzzlesState extends State<MaxScoreAllPuzzles> {
 
   BarChartData buildBarChartData() {
     List<BarChartGroupData> barGroups = <BarChartGroupData>[];
-    int sum = widget.maxScores.fold(0, (sum, bucket) => sum + bucket.sum);
-    for (Bucket bucket in widget.maxScores) {
+    int sum = maxScores.fold(0, (sum, bucket) => sum + bucket.sum);
+    for (Bucket bucket in maxScores) {
       barGroups.add(BarChartGroupData(
-        x: bucket.first + bucket.last - bucket.first,
+        x: bucket.first,
         barRods: [
           BarChartRodData(
             y: (bucket.sum.toDouble() / sum),
             colors: [Colors.lightBlueAccent, Colors.greenAccent],
           )
         ],
-        showingTooltipIndicators: [0],
       ));
     }
-
     return BarChartData(
       barGroups: barGroups,
+      titlesData: FlTitlesData(
+        show: true,
+        bottomTitles: SideTitles(
+          showTitles: true,
+          getTextStyles: (value) =>
+              const TextStyle(color: Color(0xff939393), fontSize: 10),
+          margin: 10,
+          getTitles: (double value) {
+            // This is a hack to try and show fewer lables.
+            if (value % 100 < 25) {
+              return (value ~/ 100 * 100).toString();
+            }
+            return "";
+          },
+        ),
+      ),
+    );
+  }
+}
+
+class NumberOfAnswers extends StatelessWidget {
+  final List<Bucket> numberOfAnswers;
+  NumberOfAnswers(this.numberOfAnswers);
+  @override
+  Widget build(BuildContext context) {
+    return BarChart(
+      buildBarChartData(),
+    );
+  }
+
+  BarChartData buildBarChartData() {
+    List<BarChartGroupData> barGroups = <BarChartGroupData>[];
+    int sum = numberOfAnswers.fold(0, (sum, bucket) => sum + bucket.sum);
+    for (Bucket bucket in numberOfAnswers) {
+      barGroups.add(BarChartGroupData(
+        x: bucket.first,
+        barRods: [
+          BarChartRodData(
+            y: (bucket.sum.toDouble() / sum),
+            colors: [Colors.lightBlueAccent, Colors.greenAccent],
+          )
+        ],
+      ));
+    }
+    return BarChartData(
+      barGroups: barGroups,
+      titlesData: FlTitlesData(
+        show: true,
+        bottomTitles: SideTitles(
+          showTitles: true,
+          getTextStyles: (value) =>
+              const TextStyle(color: Color(0xff939393), fontSize: 10),
+          margin: 10,
+          getTitles: (double value) {
+            // This is a hack to try and show fewer lables.
+            if (value % 50 < 5) {
+              return (value ~/ 50 * 50).toString();
+            }
+            return "";
+          },
+        ),
+      ),
     );
   }
 }
@@ -75,7 +129,23 @@ class _StatsPageState extends State<StatsPage> {
       bodyChildren = <Widget>[Text("Loading...")];
     } else {
       bodyChildren = <Widget>[
-        MaxScoreAllPuzzles(stats.maxScores),
+        Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Column(
+              children: [
+                Text("Max Score in all Puzzles"),
+                MaxScoreAllPuzzles(stats.maxScores),
+              ],
+            ),
+            Column(
+              children: [
+                Text("Number of Answers in all Puzzles"),
+                NumberOfAnswers(stats.numberOfAnswers),
+              ],
+            ),
+          ],
+        ),
       ];
     }
 
@@ -84,8 +154,7 @@ class _StatsPageState extends State<StatsPage> {
         title: Text("Stats"),
       ),
       body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
+        child: ListView(
           children: bodyChildren,
         ),
       ),
